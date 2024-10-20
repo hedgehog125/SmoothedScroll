b4ab9f905463fefc969e65bd3869c300f30a55dc
zachey <63107653+zachey01@users.noreply.github.com>
2024-10-20 14:26:18 +0500

    Fix filename

--- SmoothedScroll/models/__init__.py
+++ SmoothedScroll/models/__init__.py
@@ -1,4 +1,4 @@
 from .easing_function import EasingFunction
-from .scroll_ontrol import ScrollConfig, AppConfig, SmoothedScrollConfig, ScrollEvent
+from .scroll_control import ScrollConfig, AppConfig, SmoothedScrollConfig, ScrollEvent
 from .timer_task import TimerTask
 from .mouse_hook import LowLevelMouseProc
